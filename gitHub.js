887c9c3e21f29c25c67443ed5a64e9895079bb77
Aditya Bisoi <adityabisoi1999@gmail.com>
2021-03-12 08:06:44 +0530

    add comments

--- gitHub.js
+++ gitHub.js
@@ -1,9 +1,9 @@
 const axios = require('axios')
 
 
-const getProfile = async (url, callback) => {
-    await axios.get(url).then(res => {
-        callback(res.data)
+const getProfile = (url, callback) => {       // Two parameters, url from server.js and a callback which will be executed after obtaining data from endpoint
+    axios.get(url).then(res => {
+        callback(res.data)        // Gets URL, then sends the data to callback function which goes back to server.js
     })
 }
 
@@ -11,4 +11,4 @@ const api={
     getProfile
 }
 
-module.exports =  api
+module.exports =  api       // Function is exported to be able to be imported in server.js
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const gitHub=require('./gitHub')
 const username = 'adityabisoi'
-const url = `https://api.github.com/users/${username}`
+const url = `https://api.github.com/users/${username}`      // Define endpoint
 var fs = require('fs');
-var writeStream = fs.createWriteStream("github.xlsx");
+var writeStream = fs.createWriteStream("github.xlsx");      // To write into the file
 
-gitHub.getProfile(url,(data)=>{
-    writeData(data)
+gitHub.getProfile(url,(data)=>{     // getProfile function from gitHub.js is called with two paramenters, url and callback function
+    writeData(data)     // Pass the obtained endpoint data to writeData function below for writing in xlsx file
 })
 
 function writeData(data) {
     var header = "Username" + "\t" + "URL" + "\t" + "Name" + "\t" + "Company" + "\t" + "Blog" + "\t" + "Location" + "\t" + "Repositories" + "\t" + "Followers" + "\t" + "Following" + "\n"
     var row1 = data.login + "\t" + data.url + "\t" + data.name + "\t"+ data.company + "\t" + data.blog + "\t" + data.location + "\t" + data.public_repos + "\t" + data.followers + "\t" + data.following+ "\n";
 
-    writeStream.write(header);
-    writeStream.write(row1);
+    writeStream.write(header);      // Writes the headings in the xlsx file
+    writeStream.write(row1);      // Writes the GitHub data in the xlsx file
 
-    writeStream.close();
+    writeStream.close();      // Closes the xlsx file
 }
